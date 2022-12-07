6161cc1bccfb29d4d958a85b682a916fe258a0e1
Brett Sumser <bsumser@gmail.com>
2022-12-06 21:57:52 -0800

    new day

--- template.py
+++ template.py
@@ -25,15 +25,27 @@ def part_2(data):
     start_time = time.time()
     length = len(data)
     ans = 0
-    size = 14
 
-    for i in range(0, length - size - 1, 1):
-        print(data[i:i+size])
-        print(len(set(data[i:i+size])))
-        if (len(set(data[i:i+size])) == size):
-            print("4 non repeating at %d" % (i+size))
-            ans = i+size
-            break
+    theDict = {}
+
+    for line in data:
+        print(line)
+        if line[0] == "$":
+            if line[1] == "cd":
+                print("change dir")
+                theDict.update({'dir':'/'})
+            elif line[1] == "ls":
+                print("list dir")
+        elif line[0] == "dir":
+            print("make dir of name %s" % (line[1]))
+        elif line[0] == "cd" and line[1] == "..":
+            print("go out a dir")
+        elif line[0] == "cd" and line[1] == "/":
+            print("move to outer dir")
+        elif line[0] == "cd":
+            print("switch or make dir %s" % (line[1]))
+        elif line[0].isnumeric() == True:
+            print("make file of size %s" % (line[0]))
 
 
     print("Part 2 done in %s seconds" % (time.time() - start_time))
@@ -41,16 +53,16 @@ def part_2(data):
     return ans
 
 def parse_data():
-    #data = get_data(day=20, year=2019)
-    #data = [line.strip() for line in data.split("\n")]  # split into list
-    #data = [list(line) for line in data]
+    data = get_data(day=7, year=2022)
+    data = [line.strip() for line in data.split("\n")]  # split into list
+    data = [line.split(" ") for line in data]
     #data = [val.split("") for sublist in data for val in sublist]
 
-    my_file = open("maze.txt", "r")
-    data = my_file.read()
-    data = data.split("\n")
-    data = [list(line) for line in data]
-    my_file.close()
+    #my_file = open("maze.txt", "r")
+    #data = my_file.read()
+    #data = data.split("\n")
+    #data = [list(line) for line in data]
+    #my_file.close()
 
     return data
 
@@ -65,13 +77,12 @@ def main():
     data1 = parse_data()
     data2 = parse_data()
 
-    print(data1[2])
+    print(data1)
     print(len(data1))
 
     # ---------------Part 1------------------- #
-    ans = part_1(data1)
+    ans = part_2(data2)
     # ---------------Part 2------------------- #
-    #ans = part_2(data2)
 
     print(ans)
     pyperclip.copy(str(ans))
