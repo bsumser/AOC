d428b969a07e70dfcaf825cee1cc3d2a9f4c66d2
Brett Sumser <bsumser@gmail.com>
2022-12-14 23:20:13 -0800

    part 1 done day 15

--- template.py
+++ template.py
@@ -2,6 +2,7 @@ import re
 import time
 import math
 import pyperclip
+import numpy as np
 from aocd import get_data  # module for automating advent of code data get
 import aocFunctions
 
@@ -14,66 +15,34 @@ def part_1(data):
     print(len(data))
     start_time = time.time()
     ans = 0
-    i = 0
-    monkeys = [[],[],[],[],[],[],[],[],]
-    monk_inspect = [0, 0, 0, 0, 0, 0, 0, 0]
-    #monkeys = [[],[],[],[]]
-    #monk_inspect = [0, 0, 0, 0]
+    min_x = 0
+    max_x = 0
+    cover_set = set()
+    numpy_data = np.array(data)
+    min_x = min(min(numpy_data[:,0]), min(numpy_data[:,2]))
+    max_x = max(max(numpy_data[:,0]), max(numpy_data[:,2]))
+    print("min x is %d" % min_x)
+    print("max x is %d" % max_x)
+    for beacon in data:
+        x1 = beacon[0]
+        x2 = beacon[2]
+        y1 = beacon[1]
+        y2 = beacon[3]
+        y = 2000000
+        offset = 9000000
+        beacon_dist = abs(x1 - x2) + abs(y1 - y2)
+        #print(beacon_dist)
 
-    op = {'+': lambda x, y: x + y,
-          '*': lambda x, y: x * y,
-          '-': lambda x, y: x - y,
-          '/': lambda x, y: x / y}
+        for x in range(min_x - offset, max_x+offset, 1):
+            if (x == x2 and y == y2):
+                continue
+            check_dist = abs(x1 - x) + abs(y1 - y)
+            if (check_dist <= beacon_dist):
+                cover_set.add(x)
+    ans = len(cover_set)
 
-    superMod = 19 * 5 * 11 * 17 * 7 * 13 * 3 * 2
+    assert ans == 4748135
 
-    for start_index in range(1, len(data), 6):
-        for val in data[start_index]:
-            monkey_val = int(data[start_index - 1][0])
-            print(val)
-            monkeys[monkey_val].append(int(val))
-    for line in monkeys:
-        print(line)
-
-
-    for monk_round in range(1, 10001, 1):
-        for i in range(0, len(data), 6):
-            monkey = int(data[i][0])
-            cur_monk = monkeys[monkey]
-            ops_index = data[i+2]
-            test_index = int(data[i+3][0])
-            true_index = int(data[i+4][0])
-            false_index = int(data[i+5][0])
-            cur_monk_copy = cur_monk[:]
-            for val in cur_monk_copy:
-                #print("MONKEY %d ITEM %d" % (monkey, val))
-                monk_inspect[monkey] += 1
-                if (ops_index[-1] == "old"):
-                    #print("Worry level is %c by %d to %d." % (ops_index[1], val, val))
-                    result = op[ops_index[1]](val, val)
-                    result = result % superMod
-                    if (result % test_index == 0):
-                        monkeys[true_index].append(result)
-                        cur_monk.remove(val)
-                    elif (result % test_index != 0):
-                        monkeys[false_index].append(result)
-                        cur_monk.remove(val)
-                elif (ops_index[-1] != "old"):
-                    #print("Worry level is %c by %d to %s." % (ops_index[1], val, int(ops_index[2])))
-                    result = op[ops_index[1]](val, int(ops_index[2]))
-                    result = result % superMod
-                    if (result % test_index == 0):
-                        monkeys[true_index].append(result)
-                        cur_monk.remove(val)
-                    elif (result % test_index != 0):
-                        monkeys[false_index].append(result)
-                        cur_monk.remove(val)
-        #print("ROUND %d END" % (monk_round))
-        #print(monkeys)
-
-    monk_inspect.sort(reverse=True)
-    print(monk_inspect)
-    ans = monk_inspect[0] * monk_inspect[1]
     print("Part 1 done in %s seconds" % (time.time() - start_time))
     print("Part 1 answer is: %d" % ans)
     return ans
@@ -91,66 +60,40 @@ def part_2(data):
     return ans
 
 def parse_data():
-    data = get_data(day=11, year=2022)
-    #my_file = open("sample.txt", "r")
-    #data = my_file.read()
-    data = data.split("\n\n")
-    #my_file.close()
+    #data = get_data(day=11, year=2022)
+    my_file = open("input.txt", "r")
+    data = my_file.read()
+    data = data.split("\n")
+    my_file.close()
     data = list(map(lambda line: line.replace("Monkey ", ""), data))
-    data = list(map(lambda line: line.replace("  ", " "), data))
-    data = list(map(lambda line: line.replace("Starting items", ""), data))
-    data = list(map(lambda line: line.replace("Operation: new =", ""), data))
-    data = list(map(lambda line: line.replace("Test: divisible by", ""), data))
-    data = list(map(lambda line: line.replace("If true: throw to monkey", ""), data))
-    data = list(map(lambda line: line.replace("If false: throw to monkey", ""), data))
-    data = list(map(lambda line: line.replace(",", ""), data))
+    data = list(map(lambda line: line.replace("Sensor at x=", ""), data))
+    data = list(map(lambda line: line.replace(", y=", " "), data))
+    data = list(map(lambda line: line.replace("closest beacon is at x=", ""), data))
     data = list(map(lambda line: line.replace(":", ""), data))
-    data = list(map(lambda line: line.replace("  ", " "), data))
 
-    data = [line.split("\n") for line in data]
-    data = [val.strip() for line in data for val in line]
+
     data = [line.split(" ") for line in data]
-
-
-    for line in data:
-        print(line)
-
-    return data
-
-def parse_sample_data():
-    with open('sample.txt') as f:
-        data = f.readlines()
-    f.close()
-    data = list(map(lambda line: line.replace("Monkey ", ""), data))
-    data = list(map(lambda line: line.replace("  ", " "), data))
-    data = list(map(lambda line: line.replace("Starting items", ""), data))
-    data = list(map(lambda line: line.replace("Operation: new =", ""), data))
-    data = list(map(lambda line: line.replace("Test: divisible by", ""), data))
-    data = list(map(lambda line: line.replace("If true: throw to monkey", ""), data))
-    data = list(map(lambda line: line.replace("If false: throw to monkey", ""), data))
-    data = list(map(lambda line: line.replace(",", ""), data))
-    data = list(map(lambda line: line.replace(":", ""), data))
-    data = list(map(lambda line: line.replace("  ", " "), data))
-
-    data = [line.split("\n") for line in data]
-    data = [val.strip() for line in data for val in line]
-    data = [line.split(" ") for line in data]
-    data = [ele for ele in data if ele != ['']]
+    data = [line for line in data if line != ['']]
     print(data)
+    data = [list(map(int, line)) for line in data]
+    #data = [line.split("\n") for line in data]
+    #data = [val.strip() for line in data for val in line]
+    #data = [line.split(" ") for line in data]
+
 
     return data
+
 
 def main():
     ans = 0
-    data1 = parse_sample_data()
-    #data2 = parse_data()
+    data1 = parse_data()
+    data2 = parse_data()
 
     # ---------------Part 1------------------- #
     ans1 = part_1(data1)
     # ---------------Part 2------------------- #
-    #ans2 = part_2(data2)
+    ans2 = part_2(data2)
 
-    #total_ans = str(ans1) + str(ans2)
 
     print(ans)
     pyperclip.copy(ans1)
