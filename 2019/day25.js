b85a0f70dd1b8132d198b740ba9b464c9e076222
Brett Sumser <bsumser@gmail.com>
2023-06-21 15:32:34 -0700

    template files

---
+++ 2019/day25.js
@@ -0,0 +1,16 @@
+var fs = require('fs')
+var filepath = './day2.txt'
+
+var dataSplit = fs.readFileSync(filepath).toString('utf8');
+dataSplit = dataSplit.split(/\r?\n/);
+
+const part1 = () => {
+    return 0
+}
+
+const part2 = () => {
+    return 0
+}
+
+console.log(part1())
+console.log(part2())
