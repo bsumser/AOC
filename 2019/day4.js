611428570e8bc41f3fab9e4221e84fdb6c232455
Brett Sumser <bsumser@gmail.com>
2023-07-05 19:32:49 -0700

    template change

---
+++ 2019/day4.js
@@ -0,0 +1,24 @@
+var fs = require('fs');
+var filepath = './day4.txt';
+
+function getInput() {
+    var dataSplit = fs.readFileSync(filepath).toString('utf8');
+    dataSplit = dataSplit.split(/\r?\n?/);
+    
+    return dataSplit;
+}
+
+function part1() {
+    var dataSplit = getInput();
+    var ans = 0;
+    return ans;
+}
+
+function part2() {
+    var dataSplit = getInput();
+    var ans = 0;
+    return ans;
+}
+
+console.log(part1());
+console.log(part2());
