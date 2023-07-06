442f9df1b04c7a75172d4f7d61f4f3d59fa8280d
Brett Sumser <bsumser@gmail.com>
2023-07-05 19:32:43 -0700

    day4 init

--- 2019/day25.js
+++ 2019/day25.js
@@ -1,16 +1,24 @@
-var fs = require('fs')
-var filepath = './day2.txt'
+var fs = require('fs');
+var filepath = './day25.txt';
 
-var dataSplit = fs.readFileSync(filepath).toString('utf8');
-dataSplit = dataSplit.split(/\r?\n/);
+function getInput() {
+    var dataSplit = fs.readFileSync(filepath).toString('utf8');
+    dataSplit = dataSplit.split(/\r?\n?/);
 
-const part1 = () => {
-    return 0
+    return dataSplit;
 }
 
-const part2 = () => {
-    return 0
+function part1() {
+    var dataSplit = getInput();
+    var ans = 0;
+    return ans;
 }
 
-console.log(part1())
-console.log(part2())
+function part2() {
+    var dataSplit = getInput();
+    var ans = 0;
+    return ans;
+}
+
+console.log(part1());
+console.log(part2());
