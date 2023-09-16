cd917234016e0a275d8d333a3f939170ad2a6342
Brett Sumser <bsumser@gmail.com>
2023-09-16 16:43:53 -0700

    part 2

--- 2015/day3.py
+++ 2015/day3.py
@@ -41,16 +41,60 @@ def part_1(data):
     ans = len(ans)
     print(ans)
 
-    #print("Part 1 done in %s seconds" % (time.time() - start_time))
-    #print("Part 1 answer is: %d" % ans)
+    print("Part 1 done in %s seconds" % (time.time() - start_time))
+    print("Part 1 answer is: %d" % ans)
     return ans
 
 def part_2(data):
     '''Function that takes data and performs part 2'''
-    print("part 2 starting")
+    print("part 2 starting----reading %d lines of data" % len(data))
     start_time = time.time()
     ans = 0
 
+    start = (0, 0)
+    start2 = (0, 0)
+
+    Dict = {(0,0):2}
+
+    for i in range(0, len(data), 2):
+        start = list(start)
+        start2 = list(start2)
+        if data[i] == '^':
+            start[1] += 1
+        elif data[i] == 'v':
+            start[1] -= 1
+        elif data[i] == '<':
+            start[0] -= 1
+        elif data[i] == '>':
+            start[0] += 1
+
+        if data[i+1] == '^':
+            start2[1] += 1
+        elif data[i+1] == 'v':
+            start2[1] -= 1
+        elif data[i+1] == '<':
+            start2[0] -= 1
+        elif data[i+1] == '>':
+            start2[0] += 1
+
+        start = tuple(start)
+        start2 = tuple(start2)
+
+        if start in Dict.keys():
+            Dict[start] += 1
+            print("%s hit again value is %d" % start, Dict[start])
+        else:
+            Dict[start] = 1
+
+        if start2 in Dict.keys():
+            Dict[start2] += 1
+            print("%s hit again value is %d" % start2, Dict[start2])
+        else:
+            Dict[start2] = 1
+
+    ans = list(filter(lambda x: x >= 0, list(Dict.values())))
+    ans = len(ans)
+    print(ans)
 
     print("Part 2 done in %s seconds" % (time.time() - start_time))
     print("Part 2 answer is: %d" % ans)
@@ -82,7 +126,7 @@ def main():
     ans2 = part_2(data2)
 
 
-    print(ans1)
+    #print(ans1)
     print(ans2)
     #pyperclip.copy(ans2)
     return 0
