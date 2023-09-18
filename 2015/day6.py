78852c2d9d3940108d726c6329dd15bb3b1b412a
Brett Sumser <bsumser@gmail.com>
2023-09-18 12:34:25 -0700

    part 1 done

--- 2015/day6.py
+++ 2015/day6.py
@@ -13,8 +13,29 @@ def part_1(data):
     ans = 0
     start_time = time.time()
 
-    lights = [["-"] * 1000 for i in range(1000)]
+    lights = [[-1] * 1000 for i in range(1000)]
 
+    for line in data:
+        start_row = int(line[2])
+        start_col = int(line[1])
+        end_row = int(line[4])
+        end_col = int(line[3])
+
+        print("%sing lights from %d,%d to %d,%d" %
+              (line[0], start_row, end_row, start_col, end_col))
+
+        for i in range (start_row, end_row+1):
+            for j in range (start_col, end_col+1):
+                match line[0]:
+                    case 'toggle':
+                        lights[i][j] *= -1
+                    case 'off':
+                        if lights[i][j] == 1:
+                            lights[i][j] = -1
+                    case 'on':
+                        if lights[i][j] == -1:
+                            lights[i][j] = 1
+    ans = sum(x.count(1) for x in lights)
 
 
     print("Part 1 done in %s seconds" % (time.time() - start_time))
@@ -40,6 +61,7 @@ def parse_data():
     data = data.strip("\n\n")
     data = data.replace(" through", "")
     data = data.replace("turn ", "")
+    data = data.replace(",", " ")
     data = data.split("\n")
     data = [x.split(" ") for x in data]
 
@@ -57,7 +79,7 @@ def main():
     # ---------------Part 1------------------- #
     ans1 = part_1(data1)
     # ---------------Part 2------------------- #
-    ans2 = part_2(data2)
+    #ans2 = part_2(data2)
 
 
     print(ans1)
