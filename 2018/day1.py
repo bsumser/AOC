250f6c04e7454a581043d9fa1f0c54b73747195a
Brett Sumser <bsumser@gmail.com>
2023-10-25 15:47:16 -0700

    templates

---
+++ 2018/day1.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+import time
+import re
+import argparse
+from functools import reduce
+#import pyperclip
+#from aocd import get_data  # module for automating advent of code data get
+
+#https://aocpercenter.marcolussetti.com/
+
+def part_1(data):
+    '''Function that takes data and performs part 1'''
+    print("part 1 starting----reading %d lines of data" % len(data))
+    ans = 0
+    start_time = time.time()
+
+
+
+    print("Part 1 done in %s seconds" % (time.time() - start_time))
+    print("Part 1 answer is: %d\n" % ans)
+    return ans
+
+def part_2(data):
+    '''Function that takes data and performs part 2'''
+    print("part 2 starting----reading %d lines of data" % len(data))
+    start_time = time.time()
+    ans = 0
+
+
+    print("Part 2 done in %s seconds" % (time.time() - start_time))
+    print("Part 2 answer is: %d\n" % ans)
+    return ans
+
+def parse_data():
+    #open file and count lines
+    file_name = "./day.txt"
+    lines = open(file_name, 'r').readlines()
+    num_lines = len(lines)
+    print("parsing data for ----reading %d lines of data\n" % num_lines)
+
+    #open file and read in data
+    my_file = open(file_name, "r")
+    data = my_file.read()
+    my_file.close()
+
+    #parse data
+    #data = data.strip("\n")
+    #data = data.replace(" ", "")
+    #data = data.split(",")
+    #data = [re.split(r"([A-Z])",line) for line in data]
+    #data = [val for sublist in data for val in sublist if val]
+
+    #print(data)
+    return data
+
+
+def main():
+    ans1 = 0
+    ans2 = 0
+    data1 = parse_data()
+    data2 = parse_data()
+
+    # ---------------Part 1------------------- #
+    ans1 = part_1(data1)
+    # ---------------Part 2------------------- #
+    ans2 = part_2(data2)
+
+
+    #pyperclip.copy(ans2)
+    return 0
+main()
