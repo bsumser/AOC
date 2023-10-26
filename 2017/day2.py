6e8143d4b31bd02cc48bb9426f2123e93034579f
Brett Sumser <bsumser@gmail.com>
2023-10-26 14:27:07 -0700

    part1 done

---
+++ 2017/day2.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+import time
+import re
+import argparse
+from functools import reduce
+#import pyperclip
+#from aocd import get_data  # module for automating advent of code data get
+
+#https://aocpercenter.marcolussetti.com/
+#
+#started at 1698354885
+#finished at
+
+def part_1(data):
+    '''Function that takes data and performs part 1'''
+    print("part 1 starting----reading %d lines of data" % len(data))
+    ans = 0
+    start_time = time.time()
+
+    for row in data:
+        row = [eval(x) for x in row]
+        ans += max(row) - min(row)
+
+    print("Part 1 done in %s seconds" % (time.time() - start_time))
+    print("Part 1 answer is: %d\n" % ans)
+    return ans
+
+def part_2(data):
+    '''Function that takes data and performs part 2'''
+    print("part 2 starting----reading %d lines of data" % len(data))
+    start_time = time.time()
+    ans = 0
+
+
+    print("Part 2 done in %s seconds" % (time.time() - start_time))
+    print("Part 2 answer is: %d\n" % ans)
+    return ans
+
+def parse_data():
+    #open file and count lines
+    file_name = "./day2.txt"
+    lines = open(file_name, 'r').readlines()
+    num_lines = len(lines)
+    print("parsing data for ----reading %d lines of data\n" % num_lines)
+
+    #open file and read in data
+    my_file = open(file_name, "r")
+    data = my_file.read()
+    my_file.close()
+
+    #parse data
+    data = data.replace("\t", " ")
+    data = data.split("\n")
+    data = [row.split(" ") for row in data]
+    data = [row for row in data if row != ['']]
+
+
+    print(data)
+    return data
+
+
+def main():
+    #track time taken to get working
+    part1start = 1698354885
+    part2start = 0
+    star1 = (1698355549 - part1start) / 3600
+    star2 = (int(time.time()) - part2start) / 3600
+
+    ans1 = 0
+    ans2 = 0
+
+    data1 = parse_data()
+    data2 = parse_data()
+
+    # ---------------Part 1------------------- #
+    ans1 = part_1(data1)
+    # ---------------Part 2------------------- #
+    ans2 = part_2(data2)
+
+
+    #pyperclip.copy(ans2)
+    return 0
+main()
