b3f90dea7732df5d1d7cb8aaf9fce96e795015d8
Brett Sumser <bsumser@gmail.com>
2023-10-29 13:57:48 -0700

    part 2 done

--- 2017/day3.py
+++ 2017/day3.py
@@ -88,77 +88,92 @@ def part_2(data):
     start_time = time.time()
     ans = 0
 
-    x_i = 200
-    y_i = 200
+    num = 325489
 
-    buckets = [[0 for col in range(400)] for row in range(400)]
-    print(buckets)
+    (x,y,ans) = part2_helper(num)
 
     print("Part 2 done in %s seconds" % (time.time() - start_time))
     print("Part 2 answer is: %d\n" % ans)
     return ans
 
 def part2_helper(num):
-    x = 0
-    y = 0
+    x = 1000
+    y = 1000
     cur = 1
     inc = 0
 
+    size = 2000
+    
+    buckets = [[0 for col in range(size)] for row in range(size)]
+    buckets[1000][1000] = 1
+    print(buckets)
+
     move_list = [1, 1, 2, 2, 2]
     dir_list = ["x+", "y+", "x-", "y-", "x++"]
+    neighbors = (-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (-1, 1), (1, -1), (1, 1)
 
-    while (cur <= num):
+    while (num >= cur):
         for i in range (0, len(move_list)):
-            print(move_list[i], dir_list[i])
+            #print(move_list[i], dir_list[i])
             match dir_list[i]:
                 case "x+":
                     for j in range (0, move_list[i]):
-                        cur += 1
                         x += 1
-                        print("\t i=%d %d,%d" % (i,x,y))
-                        if (cur == num):
+                        for near in neighbors:
+                            buckets[x][y] += buckets[x+near[0]][y+near[1]]
+                        cur = buckets[x][y]
+                        print("\t cur=%d %d,%d" % (cur,x,y))
+                        if (cur > num):
                             return (x,y,cur)
                 case "y+":
                     for j in range (0, move_list[i] + inc):
-                        cur += 1
                         y += 1
-                        print("\t i=%d %d,%d" % (i,x,y))
-                        if (cur == num):
+                        for near in neighbors:
+                            buckets[x][y] += buckets[x+near[0]][y+near[1]]
+                        cur = buckets[x][y]
+                        print("\t cur=%d %d,%d" % (cur,x,y))
+                        if (cur > num):
                             return (x,y,cur)
                 case "x-":
                     for j in range (0, move_list[i] + inc):
-                        cur += 1
                         x -= 1
-                        print("\t i=%d %d,%d" % (i,x,y))
-                        if (cur == num):
+                        for near in neighbors:
+                            buckets[x][y] += buckets[x+near[0]][y+near[1]]
+                        cur = buckets[x][y]
+                        print("\t cur=%d %d,%d" % (cur,x,y))
+                        if (cur > num):
                             return (x,y,cur)
                 case "y-":
                     print("check = %d" % (move_list[i] + inc))
                     for j in range (0, move_list[i] + inc):
-                        cur += 1
                         y -= 1
-                        print("\t i=%d %d,%d" % (i,x,y))
-                        if (cur == num):
+                        for near in neighbors:
+                            buckets[x][y] += buckets[x+near[0]][y+near[1]]
+                        cur = buckets[x][y]
+                        print("\t cur=%d %d,%d" % (cur,x,y))
+                        if (cur > num):
                             return (x,y,cur)
                 case "x++":
                     print("check = %d" % (move_list[i] + inc))
                     for j in range (0, move_list[i] + inc):
-                        cur += 1
                         x += 1
-                        print("\t i=%d %d,%d" % (i,x,y))
-                        if (cur == num):
+                        for near in neighbors:
+                            buckets[x][y] += buckets[x+near[0]][y+near[1]]
+                        cur = buckets[x][y]
+                        print("\t cur=%d %d,%d" % (cur,x,y))
+                        if (cur > num):
                             return (x,y,cur)
             print("\t%d" % (cur))
             if (i == len(move_list) - 1):
                 i = 0
                 inc += 2
+    print(buckets)
     return (x,y,cur)
 
 def part2_idx_check(x, y, buckets):
-    neighbors = (-1, 0), (1, 0), (0, -1), (0, 1)
+    neighbors = (-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (-1, 1), (1, -1), (1, 1)
     for near in neighbors:
-        buckets[x][y] += buckets[x+near[0]][y+near[11]]
-
+        buckets[x][y] += buckets[x+near[0]][y+near[1]]
 
 def parse_data():
     #open file and count lines
@@ -182,11 +197,6 @@ def parse_data():
 
 def main():
     #track time taken to get working
-    part1start = 1698356551
-    part2start = 0
-    part2finish = 0
-    star1 = (part2start - part1start) / 60
-    star2 = (part2finish - part2start) / 60
 
     ans1 = 0
     ans2 = 0
@@ -200,8 +210,5 @@ def main():
     ans2 = part_2(data2)
 
 
-    print("current timestamp is %d\n" % time.time())
-    print("part 1 finished in %d mins\n" % (star1))
-    print("part 2 finished in %d mins\n" % (star2))
     return 0
 main()
