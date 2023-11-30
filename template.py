3f937e27b3a2b5b06d1ea7c6c3b45317f9f00396
Brett Sumser <bsumser@gmail.com>
2023-11-29 22:11:24 -0800

    comment for code placement

--- template.py
+++ template.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 import time
-import re
-import argparse
-from functools import reduce
+#import re
+#import argparse
+#from functools import reduce
 #import pyperclip
 #from aocd import get_data  # module for automating advent of code data get
 
@@ -14,8 +14,15 @@ def part_1(data):
     ans = 0
     start_time = time.time()
 
+   '''-------------------------------PART 1 CODE GOES HERE--------------------------------------''' 
 
 
+
+   
+   
+   
+   
+   '''-------------------------------PART 1 CODE ENDS HERE--------------------------------------''' 
     print("Part 1 done in %s seconds" % (time.time() - start_time))
     print("Part 1 answer is: %d\n" % ans)
     return ans
@@ -25,13 +32,22 @@ def part_2(data):
     print("part 2 starting----reading %d lines of data" % len(data))
     start_time = time.time()
     ans = 0
+   '''-------------------------------PART 2 CODE GOES HERE--------------------------------------''' 
+
+
+
+   
+   
+   
+   
+   '''-------------------------------PART 2 CODE ENDS HERE--------------------------------------''' 
 
 
     print("Part 2 done in %s seconds" % (time.time() - start_time))
     print("Part 2 answer is: %d\n" % ans)
     return ans
 
-def parse_data():
+def parse_data_part_1():
     #open file and count lines
     file_name = "./day.txt"
     lines = open(file_name, 'r').readlines()
@@ -50,15 +66,36 @@ def parse_data():
     #data = [re.split(r"([A-Z])",line) for line in data]
     #data = [val for sublist in data for val in sublist if val]
 
-    #print(data)
+    #print(data[0])
     return data
 
+def parse_data_part_2():
+    #open file and count lines
+    file_name = "./day.txt"
+    lines = open(file_name, 'r').readlines()
+    num_lines = len(lines)
+    print("parsing data for ----reading %d lines of data\n" % num_lines)
+
+    #open file and read in data
+    my_file = open(file_name, "r")
+    data = my_file.read()
+    my_file.close()
+
+    #parse data
+    #data = data.strip("\n")
+    #data = data.replace(" ", "")
+    #data = data.split(",")
+    #data = [re.split(r"([A-Z])",line) for line in data]
+    #data = [val for sublist in data for val in sublist if val]
+
+    #print(data[0])
+    return data
 
 def main():
     ans1 = 0
     ans2 = 0
-    data1 = parse_data()
-    data2 = parse_data()
+    data1 = parse_data_part_1()
+    data2 = parse_data_part_2()
 
     # ---------------Part 1------------------- #
     ans1 = part_1(data1)
