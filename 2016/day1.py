21c446aad705577766cbcefc1473ac648712664a
Brett Sumser <bsumser@gmail.com>
2023-11-30 17:24:17 -0800

    added stuff to write image

--- 2016/day1.py
+++ 2016/day1.py
@@ -13,6 +13,13 @@ def part_1(data):
     print("part 1 starting----reading %d lines of data" % len(data))
     ans = 0
     start_time = time.time()
+    
+    #--------------------------------SETUP IMAGE VALUES-----------------------------------#
+    image_size = 300
+    offset = 50
+    grid = [ [0]*image_size for i in range(image_size)]
+    frame_num = 0
+    #-------------------------------SETUP IMAGE VALUES------------------------------------#
 
     head = "N"
     headNum = 0
@@ -30,16 +37,25 @@ def part_1(data):
 
         head = headings[headNum % 4]
 
-        if head == 'N':
-            y += move
-        elif head == 'E':
-            x += move
-        elif head == 'S':
-            y -= move
-        elif head == 'W':
-            x -= move
-        print("turn %s %d move to move %d blocks %s, %d blocks from start" %
-              (data[i], turn, move, head, abs(x) + abs(y)))
+        for i in range(0, move):
+            if head == 'N':
+                y += 1
+            elif head == 'E':
+                x += 1
+            elif head == 'S':
+                y -= 1
+            elif head == 'W':
+                x -= 1
+            print("turn %s %d move to move %d blocks %s, %d blocks from start" %
+                  (data[i], turn, move, head, abs(x) + abs(y)))
+            #-------------------------------WRITE IMAGE OF GRID-----------------------------------#
+            grid[y+offset][x+offset] += 1
+            name = "frame_" + str(frame_num) + ".ppm"
+            f = open(name, 'w')
+            get_header(image_size, image_size, f)
+            write_image(grid, image_size, image_size, f, x+offset, y+offset)
+            frame_num += 1
+            #------------------------------------------------------------------------------------#
 
     print(x,y)
     ans = abs(x) + abs(y)
@@ -55,6 +71,13 @@ def part_2(data):
     start_time = time.time()
     ans = 0
 
+    #--------------------------------SETUP IMAGE VALUES-----------------------------------#
+    image_size = 300
+    offset = 50
+    grid = [ [0]*image_size for i in range(image_size)]
+    frame_num = 0
+    #-------------------------------SETUP IMAGE VALUES------------------------------------#
+
     head = "N"
     headNum = 0
     headings = ["N", "E", "S", "W"]
@@ -71,6 +94,7 @@ def part_2(data):
 
         head = headings[headNum % 4]
 
+
         # you originally only counted the intersections you turned at into the set,
         # instead of every intersection you passed between turns
 
@@ -83,8 +107,19 @@ def part_2(data):
                 y -= 1
             elif head == 'W':
                 x -= 1
-            print("turn %s %d move to move %d blocks %s, %d blocks from start, heading is %c" %
-                  (data[i], turn, move, head, abs(x) + abs(y), head))
+            #print("turn %s %d move to move %d blocks %s, %d blocks from start, heading is %c" %
+            #      (data[i], turn, move, head, abs(x) + abs(y), head))
+            #print("writing image pixel at: %d %d" % (y+offset, x+offset))
+            
+            #-------------------------------WRITE IMAGE OF GRID-----------------------------------#
+            grid[y+offset][x+offset] += 1
+            name = "frame_" + str(frame_num) + ".ppm"
+            f = open(name, 'w')
+            get_header(image_size, image_size, f)
+            write_image(grid, image_size, image_size, f, x+offset, y+offset)
+            frame_num += 1
+            #------------------------------------------------------------------------------------#
+            
             if ( (x,y) not in s):
                 s.add( (x,y) )
             else:
@@ -97,6 +132,29 @@ def part_2(data):
     print("Part 2 done in %s seconds" % (time.time() - start_time))
     print("Part 2 answer is: %d\n" % ans)
     return ans
+
+#create ppm file header, P3, length and width, color scale max value
+def get_header(height, width, f):
+    ppm = 'P3\n{} {}\n255\n'.format(width, height)
+    f.write(ppm)
+    return f
+
+def write_image(grid, height, width, f, x, y):
+    """Function to write an image file in ppm format
+
+    """
+
+    for row in range(height):
+        for col in range(width):
+            if (row == y and col == x):
+                f.write("0 0 0")
+            elif (grid[row][col] == 0):
+                f.write("255 255 255")
+            elif (grid[row][col] == 1):
+                f.write("128 128 128")
+            else:
+                f.write("128 128 128")
+            f.write('\n')
 
 def parse_data():
     #open file and count lines
@@ -130,7 +188,7 @@ def main():
     # ---------------Part 1------------------- #
     ans1 = part_1(data1)
     # ---------------Part 2------------------- #
-    ans2 = part_2(data2)
+    #ans2 = part_2(data2)
 
 
     #pyperclip.copy(ans2)
