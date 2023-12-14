9bf6ed3b16cff17264ae1930e02e0b14ef4c824b
Brett Sumser <bsumser@gmail.com>
2023-12-13 23:14:24 -0800

    part 1 done 2 is broken

--- 2023/day12.py
+++ 2023/day12.py
@@ -1,8 +1,15 @@
 import time
+<<<<<<< HEAD
 #import re
 ##from scanf import scanf #https://pypi.org/project/scanf/
 #import math
 #import numpy as np
+=======
+import re
+#from scanf import scanf #https://pypi.org/project/scanf/
+import math
+import numpy as np
+>>>>>>> 8c57845 (part 1 done 2 is broken)
 
 # module for automating advent of code data get
 # https://github.com/wimglenn/advent-of-code-data
@@ -19,9 +26,23 @@ def part_1(data):
     start_time = time.time()
 
     '''-------------------------------PART 1 CODE GOES HERE--------------------------------------'''
+<<<<<<< HEAD
     
     
     
+=======
+    for line in data:
+        string = line[0]
+        nums = line[1].split(",")
+        nums = [int(val) for val in nums]
+        #print(string, nums)
+        stringPoss = []
+        string_recurse(string, 0, stringPoss)
+        stringPoss = set(stringPoss)
+        for poss in stringPoss:
+            ans += string_check(poss, nums)
+
+>>>>>>> 8c57845 (part 1 done 2 is broken)
     '''-------------------------------PART 1 CODE ENDS HERE--------------------------------------'''
     end_time = time.time() - start_time
     print("Part 1 done in %s seconds" % (end_time))
@@ -29,6 +50,41 @@ def part_1(data):
 
     return (ans, end_time)
 
+<<<<<<< HEAD
+=======
+def string_recurse(string, index, stringPoss):
+    if (index == len(string)):
+        stringPoss.append(string)
+    else:
+        stringList = list(string)
+        if (stringList[index] == '#' or stringList[index] == '.'):
+            base_case = ''.join(stringList)
+            string_recurse(base_case, index+1, stringPoss)
+        elif (stringList[index] == '?'):
+            stringList[index] = '#'
+            hash_case = ''.join(stringList)
+            string_recurse(hash_case, index+1, stringPoss)
+
+            stringList[index] = '.'
+            dot_case = ''.join(stringList)
+            string_recurse(dot_case, index+1, stringPoss)
+
+def string_check(string, nums):
+    answers = string.split('.')
+    answers = [line for line in answers if line != '']
+    #print(answers)
+    if (len(answers) != len(nums)):
+        #print("invalid string with %s nums %s" % (string, nums))
+        #print("invalid on length")
+        return 0
+    for i in range(0, len(answers)):
+        if (len(answers[i]) != nums[i]):
+            #print("invalid string with %s nums %s" % (string, nums))
+            #print(len(answers[i]), nums[i])
+            return 0
+    print("valid string with %s nums %s" % (string, nums))
+    return 1
+>>>>>>> 8c57845 (part 1 done 2 is broken)
 
 def part_2(data):
     '''Function that takes data and performs part 2'''
@@ -48,10 +104,16 @@ def part_2(data):
     print("Part 2 answer is: %d\n" % ans)
     return (ans, end_time)
 
+<<<<<<< HEAD
 
 def parse_data():
     #open file and count lines
     file_name = "./day12s.txt"
+=======
+def parse_data():
+    #open file and count lines
+    file_name = "./day12.txt"
+>>>>>>> 8c57845 (part 1 done 2 is broken)
     lines = open(file_name, 'r').readlines()
     num_lines = len(lines)
     print("parsing data for ----reading %d lines of data\n" % num_lines)
@@ -65,7 +127,12 @@ def parse_data():
     '''-------------------------------PARSE BLOCK START------------------------------------------'''
 
     # split on newline
+<<<<<<< HEAD
     data = data.split("\n\n")
+=======
+    data = data.split("\n")
+    data = [line.split() for line in data]
+>>>>>>> 8c57845 (part 1 done 2 is broken)
     #for i in range(0, len(data)):
     #    data[i] = list(map(int, data[i]))
     
@@ -93,9 +160,13 @@ def parse_data():
     '''-------------------------------PARSE BLOCK END--------------------------------------------'''
 
     # print check
+<<<<<<< HEAD
     for line in data:
         print(line)
         print("\n")
+=======
+    print(data)
+>>>>>>> 8c57845 (part 1 done 2 is broken)
     return data
 
 def check_answer(answer):
@@ -118,7 +189,11 @@ def main():
     data2 = parse_data()
 
     # ---------------Part 1------------------- #
+<<<<<<< HEAD
     #ans1, part1time = part_1(data1)
+=======
+    ans1, part1time = part_1(data1)
+>>>>>>> 8c57845 (part 1 done 2 is broken)
     # ---------------Part 2------------------- #
     ans2, part2time = part_2(data2)
 
