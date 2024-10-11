d77383fbebb48e492627481442d1dc202ddd4e1d
Brett Sumser <bsumser@gmail.com>
2024-10-11 11:58:21 -0700

    part1 done

---
+++ 2021/day6.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+import time
+#import re
+#import argparse
+#from functools import reduce
+#import pyperclip
+#from aocd import get_data  # module for automating advent of code data get
+
+#https://aocpercenter.marcolussetti.com/
+
+def part_1(data):
+    '''Function that takes data and performs part 1'''
+    print("part 1 starting----reading %d lines of data" % len(data))
+    ans = 0
+    start_time = time.time()
+
+    '''-------------------------------PART 1 CODE GOES HERE--------------------------------------''' 
+    num_days = 80
+
+    while(num_days):
+        cur_length = len(data)
+        for i in range(0, cur_length):
+            data[i] -= 1
+
+            if (data[i] == -1):
+                data.append(8)
+                data[i] = 6
+        num_days -= 1
+    ans = len(data)
+
+
+
+   
+   
+   
+   
+    '''-------------------------------PART 1 CODE ENDS HERE--------------------------------------''' 
+    print("Part 1 done in %s seconds" % (time.time() - start_time))
+    print("Part 1 answer is: %d\n" % ans)
+    return ans
+
+def part_2(data):
+    '''Function that takes data and performs part 2'''
+    print("part 2 starting----reading %d lines of data" % len(data))
+    start_time = time.time()
+    ans = 0
+    '''-------------------------------PART 2 CODE GOES HERE--------------------------------------''' 
+
+
+
+   
+   
+   
+   
+    '''-------------------------------PART 2 CODE ENDS HERE--------------------------------------''' 
+
+
+    print("Part 2 done in %s seconds" % (time.time() - start_time))
+    print("Part 2 answer is: %d\n" % ans)
+    return ans
+
+def parse_data():
+    #open file and count lines
+    file_name = "./day6.txt"
+    lines = open(file_name, 'r').readlines()
+    num_lines = len(lines)
+    print("parsing data for ----reading %d lines of data\n" % num_lines)
+
+    #open file and read in data
+    my_file = open(file_name, "r")
+    data = my_file.read()
+    my_file.close()
+
+    #parse data
+    data = data.split(",")
+    data = [int(num) for num in data]
+
+    print(data)
+    return data
+
+def main():
+    ans1 = 0
+    ans2 = 0
+    data1 = parse_data()
+    data2 = parse_data()
+
+    # ---------------Part 1------------------- #
+    ans1 = part_1(data1)
+    # ---------------Part 2------------------- #
+    ans2 = part_2(data2)
+
+
+    #pyperclip.copy(ans2)
+    return 0
+main()
