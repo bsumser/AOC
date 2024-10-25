c836cb4c9c2217a3f00199d091fd806a2be00c32
Brett Sumser <bsumser@gmail.com>
2024-10-25 09:31:11 -0700

    part2 done (bfs + set)

--- 2021/day9.py
+++ 2021/day9.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 import time
+import networkx as nx
+import matplotlib.pyplot as plt
+from collections import deque as queue
 #import re
 #import argparse
 #from functools import reduce
@@ -74,18 +77,67 @@ def part_2(data):
     # every low point is a sink.
     # find how big the network around it is
     # graph algorithms
+    G = nx.Graph()
+    for i in range(0, len(data)):
+        for j in range(0, len(data[i])):
+            check = True
+            neighbors = []
+            for coord in check_list:
+                i_1 = i + coord[0]
+                j_1 = j + coord[1]
+                if ( ( 0 <= i_1 <= len(data) - 1 and 0 <= j_1 <= len(data[i]) - 1) and ( data[i][j] > data[i_1][j_1]) ):
+                    G.add_edge(data[i][j], data[i_1][j_1])
+
+    vis = [[ False for i in range(len(data[0]))] for i in range(len(data))]
+
+    basins = []
+    for low in lows:
+        basins.append(BFS(data, vis, low[0], low[1]))
+
+    basins.sort(reverse=True)
+    print(basins)
+
+    ans = (basins[0] * basins[1] * basins[2])
 
     
     '''-------------------------------PART 2 CODE ENDS HERE--------------------------------------''' 
-
 
     print("Part 2 done in %s seconds" % (time.time() - start_time))
     print("Part 2 answer is: %d\n" % ans)
     return ans
 
+
+def BFS(grid, vis, row, col):
+    print("BFS on %d at %d,%d" % (grid[row][col], row, col))
+    check_list = [ [0, -1], [0, 1], [-1, 0], [1, 0]]
+    basin_count = set()
+    q = queue()
+
+    q.append((row, col))
+    vis[row][col] = True
+
+    while(len(q) > 0):
+        cell = q.popleft()
+        x = cell[0]
+        y = cell[1]
+        print(grid[x][y], end = " ")
+
+        for coord in check_list:
+            adjx = x + coord[0]
+            adjy = y + coord[1]
+            
+            if ( ( 0 <= adjx <= len(grid) - 1 and 0 <= adjy <= len(grid[0]) - 1) and ( grid[x][y] < grid[adjx][adjy]) and (grid[adjx][adjy] != 9)):
+                q.append((adjx, adjy))
+                basin_count.add( (grid[adjx][adjy], adjx, adjy) )
+                vis[adjx][adjy] = True
+    print("BFS on %d at %d,%d done count = %d" % (grid[row][col], row, col, len(basin_count)))
+
+    return(len(basin_count) + 1)
+
+    
 def parse_data():
     #open file and count lines
-    file_name = "./day9s.txt"
+    file_name = "./day9.txt"
     lines = open(file_name, 'r').readlines()
     num_lines = len(lines)
     print("parsing data for ----reading %d lines of data\n" % num_lines)
