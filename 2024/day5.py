d263d54fbc0c9282b1113fcdcb8c311cf97428e7
Brett Sumser <bsumser@gmail.com>
2024-12-04 23:40:23 -0800

    both parts done

--- 2024/day5.py
+++ 2024/day5.py
@@ -2,6 +2,11 @@
 import time
 import sys
 import regex
+import itertools
+import functools
+
+rule_set = set()
+
 sys.path.append('../')
 import saoc
 
@@ -12,26 +17,73 @@ def part_1(data):
     start_time = time.time()
 
     '''-------------------------------PART 1 CODE GOES HERE--------------------------------------'''
-    data = [[1,2,3], [4,5,6], [7,8,9]]
+    nums = data[0].split("\n")
+    groups = data[1].split("\n")
 
-    board = saoc.AOC_GRID(data)
-    print(board)
+    post_rules = {}
+    pre_rules = {}
+    rule_set = set()
+
+    for i in range(len(nums)):
+        nums[i] = nums[i].split("|")
+        nums[i] = [int(val) for val in nums[i]]
+        rule_set.add( (nums[i][0], nums[i][1]))
+        if (nums[i][0] not in post_rules):
+            post_rules[nums[i][0]] = [nums[i][1]]
+        else:
+            post_rules[nums[i][0]].append(nums[i][1])
+        if (nums[i][1] not in pre_rules):
+            pre_rules[nums[i][1]] = [nums[i][0]]
+        else:
+            pre_rules[nums[i][1]].append(nums[i][0])
     
-    #for i in range(3):
-    #    print(board.get_row(i))
-    #    print(board.get_col(i))
+    for i in range(len(groups)):
+        groups[i] = groups[i].split(",")
+        groups[i] = [int(val) for val in groups[i]]
+    #print(groups)
 
-    print(board.get_anti_diag(1,1))
+    passes = []
+    for group in groups:
+        if (group_checker(group, pre_rules, post_rules, rule_set)):
+            passes.append(group)
+    for group in passes:
+        ans += group[len(group)//2]
+
+                
+
     
     
     '''-------------------------------PART 1 CODE ENDS HERE--------------------------------------'''
     print("Part 1 done in %s seconds" % (time.time() - start_time))
     print("Part 1 answer is: %d\n" % ans)
-    #assert ans == 2358
+    assert ans > 111
 
     #big boy assertion
     #assert ans == 7025140
     return ans
+
+def group_checker(group, pre_rules, post_rules, rule_set):
+    for i in range(0, len(group)):
+        cur_val = group[i]
+        end_slice = set(group[i+1:])
+        start_slice = set(group[0:i])
+        
+        for val in start_slice:
+            if (cur_val, val) in rule_set:
+                return False
+
+            if (val, cur_val) in rule_set:
+                cur_key = pre_rules[cur_val]
+                if val in cur_key:
+                    pass
+        for val in end_slice:
+            if (val, cur_val) in rule_set:
+                return False
+            if (cur_val, val) in rule_set:
+                cur_key = post_rules[cur_val]
+                if val not in cur_key:
+                    return False
+    return True
 
 
 def part_2(data):
@@ -41,6 +93,46 @@ def part_2(data):
     ans = 0
 
     '''-------------------------------PART 2 CODE GOES HERE--------------------------------------'''
+    nums = data[0].split("\n")
+    groups = data[1].split("\n")
+
+    post_rules = {}
+    pre_rules = {}
+    rule_set = set()
+
+    for i in range(len(nums)):
+        nums[i] = nums[i].split("|")
+        nums[i] = [int(val) for val in nums[i]]
+        rule_set.add( (nums[i][0], nums[i][1]))
+        if (nums[i][0] not in post_rules):
+            post_rules[nums[i][0]] = [nums[i][1]]
+        else:
+            post_rules[nums[i][0]].append(nums[i][1])
+        if (nums[i][1] not in pre_rules):
+            pre_rules[nums[i][1]] = [nums[i][0]]
+        else:
+            pre_rules[nums[i][1]].append(nums[i][0])
+    
+    for i in range(len(groups)):
+        groups[i] = groups[i].split(",")
+        groups[i] = [int(val) for val in groups[i]]
+    #print(groups)
+
+    fails = []
+    re_pass = []
+    for group in groups:
+        if not (group_checker(group, pre_rules, post_rules, rule_set)):
+            fails.append(group)
+    for fail in fails:
+        i = 0
+        while i < len(fail) - 1:
+            # bubble sort up according to post idx rule
+            if fail[i+1] not in post_rules[fail[i]]:
+                fail[i], fail[i+1] = fail[i+1], fail[i]
+                i = -1
+            i += 1
+    for group in fails:
+        ans += group[len(group)//2]
 
 
     '''-------------------------------PART 2 CODE ENDS HERE--------------------------------------''' 
@@ -52,6 +144,14 @@ def part_2(data):
     #assert ans == 879274
     return ans
 
+def compare(a, b):
+    if (a,b) not in rule_set:
+        return -1
+    elif a == b:
+        return 0
+    else:
+        return 1
+
 def parse_data():
     start_time = time.time()
 
@@ -59,11 +159,10 @@ def parse_data():
     file_name = sys.argv[1]
     print(f"parsing data for {file_name}")
     data = open(file_name, "r").read()
-    data = data.split("\n")
-    data = [list(line) for line in data]
+    data = data.split("\n\n")
+    #data = [list(line) for line in data]
     #data = [[int(val) for val in line] for line in data]
 
-    print(data)
     print("Parsing done in %s seconds" % (time.time() - start_time))
     return data
 
