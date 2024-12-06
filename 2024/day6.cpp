97cf7dbead0eeeaf9a5c39521a427a0af25c877a
Brett Sumser <bsumser@gmail.com>
2024-12-06 01:52:45 -0800

    part2 done, c++ is faster than python solution

--- 2024/day6.cpp
+++ 2024/day6.cpp
@@ -1,26 +1,24 @@
 #include <bits/stdc++.h>
-#include <chrono>
 #include <string>
 
 using namespace std;
 
-int silver(vector<vector<char> > data);
-int gold(vector<vector<char> > data);
+std::set<std::array<int, 2>> silver(vector<vector<char> > data, int &initRow, int &initCol);
+int gold(vector<vector<char> > data, std::set<std::array<int, 2>> pos, int startRow, int startCol);
 vector<vector<char> > parse_data(char* filename);
+bool cycle_detect(vector<vector<char> > data, int startRow, int startCol);
 
 int main(int argc, char* argv[]) {
-
+    int initRow = 0, initCol = 0;
     vector<vector<char> > data = parse_data(argv[1]);
-    silver(data);
-    gold(data);
+    std::set<std::array<int, 2>> pos = silver(data, initRow, initCol);
+    gold(data, pos, initRow, initCol);
 
     return 0;
 }
 
-int silver(vector<vector<char> > data) {
-    auto start = std::chrono::high_resolution_clock::now();
+std::set<std::array<int, 2>> silver(vector<vector<char> > data, int &initRow, int &initCol) {
     int ans = 0;
-
     int startRow = 0;
     int startCol = 0;
 
@@ -30,6 +28,7 @@ int silver(vector<vector<char> > data) {
         for (int col = 0; col < data[0].size(); col++) {
             if (data[row][col] == '^') {
                 startRow = row, startCol = col;
+                initRow = row, initCol = col;
                 printf("start found at (%d,%d)\n", startRow, startCol);
             }
         }
@@ -56,27 +55,61 @@ int silver(vector<vector<char> > data) {
     }
     ans = pos.size();
 
-    auto end = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-    
-    cout << "Silver done in " << duration.count() << " microseconds" << endl;
     cout << "Silver answer is " << ans << endl;
-    return ans;
+    return pos;
 }
 
-int gold(vector<vector<char> > data) {
-    auto start = std::chrono::high_resolution_clock::now();
+int gold(vector<vector<char> > data, std::set<std::array<int, 2>> pos, int startRow, int startCol) {
     int ans = 0;
     
-    auto end = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+    for (const auto &loc : pos) {
+        data[loc[0]][loc[1]] = '#';
+        //printf("Checking for cycles with (%d,%d)\n", loc[0], loc[1]);
+        if (cycle_detect(data, startRow, startCol)) {
+            ans += 1;
+        }
+        data[loc[0]][loc[1]] = '.';
+    }
     
-    cout << "Gold done in " << duration.count() << " microseconds" << endl;
+    cout << "Gold answer is " << ans << endl;
     return ans;
+}
+bool cycle_detect(vector<vector<char> > data, int startRow, int startCol) {
+    int dirs[4][4] = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+    int dir = 0;
+
+    unordered_map<string, int> cycle_count;
+    int cycle_thresh = 5;
+
+    data[startRow][startCol] = '.';
+
+    while (0 <= startRow < data.size() && 0 <= startCol < data[0].size()) {
+        int nextRow = startRow + dirs[dir % 4][0], nextCol = startCol + dirs[dir%4][1];
+        if ((0 > nextRow || nextRow >= data.size() || 0 > nextCol || nextCol >= data[0].size())) {
+            break;
+        }
+        if (data[nextRow][nextCol] == '.'){
+            startRow = nextRow, startCol = nextCol;
+        }
+        
+        else if (data[nextRow][nextCol] == '#') {
+            dir += 1;
+        }
+        string key = to_string(startRow) + to_string(startCol);
+        if (cycle_count.find(key) != cycle_count.end()) {
+            cycle_count[key] += 1;
+            if (cycle_count[key] > cycle_thresh) {
+                return true;
+            }
+        }
+        else {
+            cycle_count[key] = 1;
+        }
+    }
+    return false;
 }
 
 vector<vector<char> >  parse_data(char* filename) {
-    auto start = std::chrono::high_resolution_clock::now();
     ifstream inputFile(filename); // Open file for reading
 
     vector<vector<char> > data;
@@ -84,7 +117,6 @@ vector<vector<char> >  parse_data(char* filename) {
     if (inputFile.is_open()) { 
         string line;
         while (getline(inputFile, line)) {
-            cout << line << endl; // Read and output each line
             std::vector<char> row(line.begin(), line.end());
             data.push_back(row);
         }
@@ -94,18 +126,13 @@ vector<vector<char> >  parse_data(char* filename) {
         return data;
     }
 
-    for (const auto &row : data) {
-        for (const auto &val : row) {
-            cout << val << " ";
-        }
-        cout << endl;
-    }
-    cout << endl;
+    //for (const auto &row : data) {
+    //    for (const auto &val : row) {
+    //        cout << val << " ";
+    //    }
+    //    cout << endl;
+    //}
+    //cout << endl;
     
-    auto end = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-    
-    cout << "Parsing done in " << duration.count() << " microseconds" << endl;
-
     return data;
 }
