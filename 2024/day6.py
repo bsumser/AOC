79e068e47945757ab501768da3f1830fa33849a0
Brett Sumser <bsumser@gmail.com>
2024-12-05 17:26:33 -0800

    added template for day6

---
+++ 2024/day6.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+import time
+import sys
+sys.path.append('../')
+import saoc
+
+def part_1(data):
+    '''Function that takes data and performs part 1'''
+    print("part 1 starting----reading %d lines of data" % len(data))
+    ans = 0
+    start_time = time.time()
+
+    '''-------------------------------PART 1 CODE GOES HERE--------------------------------------'''
+
+    
+    '''-------------------------------PART 1 CODE ENDS HERE--------------------------------------'''
+    print("Part 1 done in %s seconds" % (time.time() - start_time))
+    print("Part 1 answer is: %d\n" % ans)
+    return ans
+
+
+def part_2(data):
+    '''Function that takes data and performs part 2'''
+    print("part 2 starting----reading %d lines of data" % len(data))
+    start_time = time.time()
+    ans = 0
+
+    '''-------------------------------PART 2 CODE GOES HERE--------------------------------------'''
+
+    
+
+    '''-------------------------------PART 2 CODE ENDS HERE--------------------------------------''' 
+    print("Part 2 done in %s seconds" % (time.time() - start_time))
+    print("Part 2 answer is: %d\n" % ans)
+    return ans
+
+def parse_data():
+    start_time = time.time()
+    # open file and read in data
+    print(f"parsing data for {sys.argv[1]}")
+    data = open(sys.argv[1], "r").read().split("\n")
+
+    print("Parsing done in %s seconds" % (time.time() - start_time))
+    return data
+
+def main():
+    # ---------------Part 1------------------- #
+    part_1(parse_data())
+    # ---------------Part 2------------------- #
+    part_2(parse_data())
+
+    return 0
+
+if __name__ == "__main__":
+    main()
