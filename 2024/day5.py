25c1171377a680b8cf6e5c9a04a35cd45630d74c
Brett Sumser <bsumser@gmail.com>
2024-12-05 17:20:03 -0800

    cleeaned up code

--- 2024/day5.py
+++ 2024/day5.py
@@ -11,40 +11,24 @@ def part_1(data):
     start_time = time.time()
 
     '''-------------------------------PART 1 CODE GOES HERE--------------------------------------'''
-    nums = data[0]
-    groups = data[1]
-
-    post_rules = {}
-    pre_rules = {}
-    rule_set = set()
+    nums, groups, rule_set = data[0], data[1], set()
 
     for i in range(len(nums)):
         rule_set.add( (nums[i][0], nums[i][1]))
-        if (nums[i][0] not in post_rules):
-            post_rules[nums[i][0]] = [nums[i][1]]
-        else:
-            post_rules[nums[i][0]].append(nums[i][1])
-        if (nums[i][1] not in pre_rules):
-            pre_rules[nums[i][1]] = [nums[i][0]]
-        else:
-            pre_rules[nums[i][1]].append(nums[i][0])
     
     passes = list(filter(lambda group: group_checker(group, rule_set), groups))
-    for group in passes:
-        ans += group[len(group)//2]
+    ans = sum(group[len(group)//2] for group in passes)
     
     '''-------------------------------PART 1 CODE ENDS HERE--------------------------------------'''
     print("Part 1 done in %s seconds" % (time.time() - start_time))
     print("Part 1 answer is: %d\n" % ans)
     #big boy assertion
-    #assert ans == 7025140
+    #assert ans == 14346279
     return ans
 
 def group_checker(group, rule_set):
     for i in range(0, len(group)):
-        cur_val = group[i]
-        end_slice = set(group[i+1:])
-        start_slice = set(group[0:i])
+        cur_val, end_slice, start_slice = group[i], group[i+1:], group[0:i]
         
         for val in start_slice:
             if (cur_val, val) in rule_set:
@@ -63,23 +47,14 @@ def part_2(data):
     ans = 0
 
     '''-------------------------------PART 2 CODE GOES HERE--------------------------------------'''
-    nums = data[0]
-    groups = data[1]
-
-    post_rules = {}
-    pre_rules = {}
-    rule_set = set()
+    nums, groups, rule_set, post_rules = data[0], data[1], set(), {}
 
     for i in range(len(nums)):
         rule_set.add( (nums[i][0], nums[i][1]))
-        if (nums[i][0] not in post_rules):
+        if nums[i][0] not in post_rules:
             post_rules[nums[i][0]] = [nums[i][1]]
-        else:
+        elif nums[i][0] in post_rules:
             post_rules[nums[i][0]].append(nums[i][1])
-        if (nums[i][1] not in pre_rules):
-            pre_rules[nums[i][1]] = [nums[i][0]]
-        else:
-            pre_rules[nums[i][1]].append(nums[i][0])
     
     fails = list((filter(lambda group: not group_checker(group, rule_set), groups)))
     for fail in fails:
@@ -90,24 +65,20 @@ def part_2(data):
                 fail[i], fail[i+1] = fail[i+1], fail[i]
                 i = -1
             i += 1
-    for group in fails:
-        ans += group[len(group)//2]
-
+    ans = sum(group[len(group)//2] for group in fails)
 
     '''-------------------------------PART 2 CODE ENDS HERE--------------------------------------''' 
     print("Part 2 done in %s seconds" % (time.time() - start_time))
     print("Part 2 answer is: %d\n" % ans)
     #big boy assertion
-    #assert ans == 879274
+    #assert ans == 14357204
     return ans
 
 def parse_data():
     start_time = time.time()
-
     # open file and read in data
-    file_name = sys.argv[1]
-    print(f"parsing data for {file_name}")
-    data = open(file_name, "r").read().split("\n\n")
+    print(f"parsing data for {sys.argv[1]}")
+    data = open(sys.argv[1], "r").read().split("\n\n")
     nums = [[int(val) for val in rule.split('|') if val ] for rule in data[0].split("\n") if rule]
     groups = [[int(val) for val in group.split(',') if val ] for group in data[1].split("\n") if group]
     data = [nums, groups]
