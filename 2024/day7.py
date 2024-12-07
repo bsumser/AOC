4f34ee95c58c7ee7cf79d9c6642d6fcd2be0517b
Brett Sumser <bsumser@gmail.com>
2024-12-07 12:58:35 -0800

    tree build working now

--- 2024/day7.py
+++ 2024/day7.py
@@ -8,10 +8,16 @@ from functools import reduce
 import operator
 from itertools import product
 
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.children = []
+    def __repr__(self):
+        return f"Node(val='{self.val}', children={self.children})"
+
 def part_1(data):
     print("part 1 starting----reading %d lines of data" % len(data))
     ans = 0
-    '''-------------------------------PART 1 CODE GOES HERE--------------------------------------'''
     for line in data:
         cur = line[1:]
         cur_sum = sum(cur)
@@ -22,27 +28,53 @@ def part_1(data):
             if calibrate(line):
                 ans += line[0]
 
-    '''-------------------------------PART 1 CODE ENDS HERE--------------------------------------'''
     print("Part 1 answer is: %d\n" % ans)
     return ans
 
 def silver(data):
     print("part 1 starting----reading %d lines of data" % len(data))
     ans = 0
-    '''-------------------------------PART 1 CODE GOES HERE--------------------------------------'''
     #1. Create a graph of all possible combination of operators. 
     #2. DFS through the graph and test each combination until we get the sum. 
     #Part 2 was exactly the same except with one extra operator.
-    for line in data:
-        create_graph(line)
+    #for line in data:
+    #    create_graph(line[1:])
+    
+    root = create_graph([1,2,3])
+    print_tree(root)
 
-    '''-------------------------------PART 1 CODE ENDS HERE--------------------------------------'''
     print("Part 1 answer is: %d\n" % ans)
     return ans
 
 def create_graph(line):
     #create graph for all combinations of operators
-    print(f"graph for{line}")
+    
+    def build_tree(node, index):
+        if index >= len(line):
+            return
+        next_val = line[index]
+
+        add_node = Node(node.val + next_val)
+        node.children.append(add_node)
+            
+        mul_node = Node(node.val * next_val)
+        node.children.append(mul_node)
+
+        build_tree(add_node, index+1)
+        build_tree(mul_node, index+1)
+
+    root = Node(line[0])
+
+    build_tree(root, 1)
+
+    print(f"graph for{root}")
+    return root
+
+def print_tree(root):
+    if root:
+        print(root.val)
+        for child in root.children:
+            print_tree(child)
 
 def calibrate(line):
     print(f"checking {line}")
@@ -68,12 +100,10 @@ def calibrate(line):
     
 
 def part_2(data):
-    '''Function that takes data and performs part 2'''
     print("part 2 starting----reading %d lines of data" % len(data))
     start_time = time.time()
     ans = 0
 
-    '''-------------------------------PART 2 CODE GOES HERE--------------------------------------'''
     for line in data:
         cur = line[1:]
         cur_sum = sum(cur)
@@ -83,11 +113,6 @@ def part_2(data):
         else:
             if calibrate_2(line):
                 ans += line[0]
-
-    test = [6,1,2,3]
-    calibrate(test)
-
-    '''-------------------------------PART 2 CODE ENDS HERE--------------------------------------''' 
     print("Part 2 answer is: %d\n" % ans)
     return ans
 
