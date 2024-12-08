4473900091ac75250050a7220e9bf71e4491ea24
Brett Sumser <bsumser@gmail.com>
2024-12-08 00:13:02 -0800

    part2 done

--- 2024/day8.py
+++ 2024/day8.py
@@ -97,8 +97,73 @@ def gold(data):
     print("gold starting----reading %d lines of data" % len(data))
     ans = 0
 
+    check_dict = {}
+
+    for row in range(len(data)):
+        for col in range(len(data[row])):
+            cur = data[row][col]
+            if cur == '.':
+                continue
+            elif cur in check_dict:
+                check_dict[cur].append( (row,col) )
+            else:
+                check_dict[cur] = [(row,col)]
+
+    for key,val in check_dict.items():
+        print(key, val)
+
+    dist_dict = {}
+    anti_set = set()
+
+    for key in check_dict.keys():
+        coords = [val for val in check_dict[key]]
+        test_list = list(itertools.combinations(coords, 2))
+        print(f"test {test_list}")
+        for combo in test_list:
+            first = list(combo[0])
+            second = list(combo[1])
+            antis = check_line_gold(first, second, key, data)
+            for anti in antis:
+                anti_set.add(anti)
+
+    print(anti_set)
+    ans += len(anti_set)
+
     print("Gold answer is: %d\n" % ans)
     return ans
+
+def check_line_gold(first, second, char, data):
+    antis = []
+    dist = check_dist(first, second) / 2
+    slope = second[1] - first[1] / second[0] - first[0]
+    rise = (second[1] - first[1])
+    run = (second[0] - first[0])
+    print(f"    check {char} {first}, {second} slope {slope} {rise}/{run} dist {dist}")
+    a_1 = [0,0]
+    a_2 = [0,0]
+    a_1[1] = first[1] - rise
+    a_1[0] = first[0] - run
+    antis.append(tuple(a_1))
+    
+    while (0 <= a_1[0] and a_1[0] < len(data) and 0 <= a_1[1] and a_1[1] < len(data[0])):
+        antis.append(tuple(a_1))
+        a_1[1] -=  rise
+        a_1[0] -=  run
+        print(a_1)
+
+    a_2[1] = second[1] + rise
+    a_2[0] = second[0] + run
+    antis.append(tuple(a_2))
+    
+    while (0 <= a_2[0] and a_2[0] < len(data) and 0 <= a_2[1] and a_2[1] < len(data[0])):
+        antis.append(tuple(a_2))
+        a_2[1] += rise
+        a_2[0] += run
+
+    print(f"    antis {char} {first}, {second} at {a_1} and {a_2}")
+
+    return antis
+
 
 def parse_data():
     start_time = time.time()
